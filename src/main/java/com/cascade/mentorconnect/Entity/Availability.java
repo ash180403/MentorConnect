fbd56e81f19767e49793d60fa143ab9aaa78dca8
ash180403 <ashishakteri@gmail.com>
2025-01-25 23:32:40 +0530

    commit

--- src/main/java/com/cascade/mentorconnect/Entity/Availability.java
+++ src/main/java/com/cascade/mentorconnect/Entity/Availability.java
@@ -1,15 +1,12 @@
 package com.cascade.mentorconnect.Entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Getter
-@Setter
 public class Availability {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,4 +18,46 @@ public class Availability {
     @ManyToOne
     @JoinColumn(name = "mentor_id", nullable = false)
     private Mentor mentor;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Mentor getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(Mentor mentor) {
+        this.mentor = mentor;
+    }
 }
