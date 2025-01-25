996e2a22caf8caa8738ab7e0c0a6f3bc8ec64330
ash180403 <ashishakteri@gmail.com>
2025-01-25 23:32:58 +0530

    commit

---
+++ src/main/java/com/cascade/mentorconnect/Repository/AvailabilityRepository.java
@@ -0,0 +1,12 @@
+package com.cascade.mentorconnect.Repository;
+import com.cascade.mentorconnect.Entity.Availability;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
+    List<Availability> findByMentorId(Long mentorId);
+}
