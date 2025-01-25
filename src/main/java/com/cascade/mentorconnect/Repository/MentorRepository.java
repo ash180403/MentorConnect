12c816bf9f37dbcddd11fa2168249575a814594d
ash180403 <ashishakteri@gmail.com>
2025-01-25 23:33:06 +0530

    commit

--- src/main/java/com/cascade/mentorconnect/Repository/MentorRepository.java
+++ src/main/java/com/cascade/mentorconnect/Repository/MentorRepository.java
@@ -1,15 +1,15 @@
 package com.cascade.mentorconnect.Repository;
 
-import com.cascade.mentorconnect.Entity.Availability;
+import com.cascade.mentorconnect.Entity.Mentor;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MentorRepository extends jpaRepository<Mentor, Long> {
-    List<Mentor> findByExpertiseAndLocation(String expertise, String location);
-}
-    @Repository
-    public interface AvailabilityRepository extends JpaRepository<Availability, Long> {
-        List<Availability> findByMentorId(Long mentorId);
-    }
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
+	List<Mentor> findByExpertiseAndLocation(String expertise, String location);
+	}
+    
 
