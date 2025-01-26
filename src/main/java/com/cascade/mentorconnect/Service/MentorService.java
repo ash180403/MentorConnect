fd857adc52eadb3b3a9624616b1ea0122689e812
ash180403 <ashishakteri@gmail.com>
2025-01-26 22:41:53 +0530

    commit

--- src/main/java/com/cascade/mentorconnect/Service/MentorService.java
+++ src/main/java/com/cascade/mentorconnect/Service/MentorService.java
@@ -1,15 +1,15 @@
 package com.cascade.mentorconnect.Service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 import com.cascade.mentorconnect.Entity.Mentor;
 import com.cascade.mentorconnect.Repository.MentorRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class MentorService {
+
     @Autowired
     private MentorRepository mentorRepository;
 
@@ -19,5 +19,10 @@ public class MentorService {
 
     public List<Mentor> searchMentors(String expertise, String location) {
         return mentorRepository.findByExpertiseAndLocation(expertise, location);
+    }
+
+    public Mentor findMentorById(Long mentorId) {
+        Optional<Mentor> mentorOpt = mentorRepository.findById(mentorId);
+        return mentorOpt.orElse(null);
     }
 }
