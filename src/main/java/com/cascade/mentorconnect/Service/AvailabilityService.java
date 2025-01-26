30b0fcdaa38245dcd63c6b603fb9dbdeea5be1f2
ash180403 <ashishakteri@gmail.com>
2025-01-26 22:41:40 +0530

    commit

--- src/main/java/com/cascade/mentorconnect/Service/AvailabilityService.java
+++ src/main/java/com/cascade/mentorconnect/Service/AvailabilityService.java
@@ -1,18 +1,9 @@
 package com.cascade.mentorconnect.Service;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.cascade.mentorconnect.Entity.Availability;
-import com.cascade.mentorconnect.Repository.AvailabilityRepository;
+import java.util.List;
 
-@Service
-public class AvailabilityService {
-    @Autowired
-    private AvailabilityRepository availabilityRepository;
-
-    public List<Availability> getAvailability(Long mentorId) {
-        return availabilityRepository.findByMentorId(mentorId);
-    }
+public interface AvailabilityService {
+    List<Availability> getAvailability(Long mentorId);
+    Availability addAvailability(Long mentorId, Availability availability);  
 }
-
